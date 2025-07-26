5dddd58559b9903596588c50daa6ea9d1239d383
Sidequest Ju <dominik.gratz99@gmail.com>
2025-07-26 20:39:26 +0200

    Add radio flamingo and radio bollerwagen streams

---
+++ src/go.mod
@@ -0,0 +1,17 @@
+module github.com/Sidequest-Ju/Erika
+
+go 1.24.5
+
+require (
+	github.com/bwmarrin/dgvoice v0.0.0-20210225172318-caaac756e02e
+	github.com/bwmarrin/discordgo v0.29.0
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
+	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/dgvoice"
+	"github.com/bwmarrin/discordgo"
+	"go.uber.org/zap"
+)
+
+const RADIO_FLAMINGO string = "https://live.radioflamingo.at/rf"
+const RADIO_BOLLERWAGEN string = "http://player.ffn.de/radiobollerwagen.mp3"
+
+var logger *zap.SugaredLogger
+var voiceConns = make(map[string]*discordgo.VoiceConnection)
+
+func main() {
+	var err error
+	var ok bool
+	var token string
+	var log *zap.Logger
+	var discordClient *discordgo.Session
+
+	if log, err = zap.NewProduction(); err != nil {
+		panic("Failed to create logger: " + err.Error())
+	}
+
+	defer log.Sync()
+	logger = log.Sugar()
+
+	if token, ok = os.LookupEnv("TOKEN"); !ok || token == "" {
+		logger.Fatal("Env variable TOKEN not set")
+	}
+
+	if discordClient, err = discordgo.New("Bot " + token); err != nil {
+		logger.Fatal("Error creating Discord session", zap.Error(err))
+	}
+
+	discordClient.AddHandler(onMessageCreate)
+
+	if err = discordClient.Open(); err != nil {
+		logger.Fatal("Error opening Discord session", zap.Error(err))
+	}
+
+	logger.Info("Bot is now running. Press CTRL+C to exit.")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-stop
+
+	logger.Info("Shutting down bot...")
+	discordClient.Close()
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.Bot {
+		return
+	}
+
+	switch m.Content {
+	case "!bollerwagen":
+		logger.Infow("Received !bollerwagen command", "user", m.Author.Username)
+		joinAndPlay(s, m, RADIO_BOLLERWAGEN)
+	case "!flamingo":
+		logger.Infow("Received !flamingo command", "user", m.Author.Username)
+		joinAndPlay(s, m, RADIO_FLAMINGO)
+	case "!leave":
+		logger.Infow("Received !leave command", "user", m.Author.Username)
+		leaveChannel(s, m)
+	}
+}
+func joinAndPlay(s *discordgo.Session, m *discordgo.MessageCreate, streamUrl string) (err error) {
+	var guild *discordgo.Guild
+	var userChannelID string
+	var vc *discordgo.VoiceConnection
+
+	if guild, err = s.State.Guild(m.GuildID); err != nil {
+		err = fmt.Errorf("failed to get guild '%s': %v", m.GuildID, err)
+		return
+	}
+
+	for _, vs := range guild.VoiceStates {
+		if vs.UserID == m.Author.ID {
+			userChannelID = vs.ChannelID
+			break
+		}
+	}
+
+	if userChannelID == "" {
+		s.ChannelMessageSend(m.ChannelID, "You're not in a voice channel.")
+		return
+	}
+
+	if vc, err = s.ChannelVoiceJoin(m.GuildID, userChannelID, false, true); err != nil {
+		err = fmt.Errorf("failed to join voice channel: %v", err)
+		s.ChannelMessageSend(m.ChannelID, "Failed to join the voice channel.")
+		return
+	}
+
+	voiceConns[m.GuildID] = vc
+	logger.Infow("Joined voice channel", "guildID", m.GuildID, "channelID", userChannelID)
+
+	go streamAudio(vc, streamUrl)
+
+	return
+}
+
+func leaveChannel(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if vc, ok := voiceConns[m.GuildID]; ok {
+		vc.Disconnect()
+		delete(voiceConns, m.GuildID)
+		logger.Infow("Disconnected from voice channel", "guildID", m.GuildID)
+		s.ChannelMessageSend(m.ChannelID, "Left the voice channel.")
+	} else {
+		logger.Warnw("Leave command received but not in a voice channel", "guildID", m.GuildID)
+		s.ChannelMessageSend(m.ChannelID, "Not in a voice channel.")
+	}
+}
+
+func streamAudio(vc *discordgo.VoiceConnection, streamUrl string) (err error) {
+	var ffmpegArgs []string
+	var cmd *exec.Cmd
+	var stdout io.ReadCloser
+	var reader *bufio.Reader
+	var buffer []int16
+	var sendChan chan []int16
+
+	vc.Speaking(true)
+	defer vc.Speaking(false)
+
+	ffmpegArgs = []string{
+		"-i", streamUrl,
+		"-f", "s16le",
+		"-ar", "48000",
+		"-ac", "2",
+		"-loglevel", "quiet",
+		"pipe:1",
+	}
+
+	cmd = exec.Command("ffmpeg", ffmpegArgs...)
+
+	if stdout, err = cmd.StdoutPipe(); err != nil {
+		err = fmt.Errorf("Failed to get ffmpeg stdout: %w", err)
+		return
+	}
+
+	cmd.Stderr = os.Stderr
+
+	if err = cmd.Start(); err != nil {
+		err = fmt.Errorf("Failed to start ffmpeg: %w", err)
+		return
+	}
+
+	reader = bufio.NewReader(stdout)
+	buffer = make([]int16, 960*2) // 20ms of stereo audio at 48kHz
+	sendChan = make(chan []int16, 2)
+
+	go dgvoice.SendPCM(vc, sendChan)
+
+	for {
+		for i := range buffer {
+			var sample int16
+
+			if err = binary.Read(reader, binary.LittleEndian, &sample); err != nil {
+				err = fmt.Errorf("Finished reading: %w", err)
+				return
+			}
+
+			buffer[i] = sample
+		}
+
+		sendChan <- buffer
+	}
+}
